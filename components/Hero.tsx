9da2a34280c9a10526c245eb750c7eed8d68272e
hisham rehab <hishamrehab309@gmail.com>
2025-01-31 15:16:47 +0200

    Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,7 @@
+import ShimmerButton from "./ui/ShimmerButton";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
@@ -18,16 +21,36 @@ const Hero = () => {
         />
       </div>
 
-      <div className="h-screen  w-full dark:bg-black-100  bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen  w-full dark:bg-black-100  bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.05] flex items-center justify-center absolute top-0 left-0 ">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
-      <div className="flex justify-center ">
-        <div className="max-w-[89vw]">
-          <h2 className="uppercase">
-            Dynamic Web Magic With Next js
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest tex-xs text-center text-blue-100 max-w-80">
+            Hi, I'm Eng Hisham Rehab, <br />I build responsive and
+            Dynamic Web Magic With React and Next.js
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into seamless Experiences"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Welcome to the world of dynamic web magic, where concepts
+            come alive in seamless experiences. With React and
+            Next.js, I bring you a captivating blend of creativity and
+            innovation, where lines of code dance into a symphony of
+            interactive web experiences.
+          </p>
+
+          <a href="/about">
+            <ShimmerButton
+              title="Show My Work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
