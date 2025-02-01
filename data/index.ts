d03f37002eff34c2da0818bbbc103d7d84ccf5d7
hisham rehab <hishamrehab309@gmail.com>
2025-02-01 19:31:02 +0200

    Projects Section

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,4 @@
+import img from "../public/images/movie-cover.png";
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
@@ -30,9 +31,9 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "My tech stack",
-    description: "I constantly try to improve",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    title: "",
+    description: "",
+    className: "lg:col-span-1 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-center",
     img: "",
@@ -74,48 +75,83 @@ export const gridItems = [
 ];
 
 export const projects = [
+  // {
+  //   id: 1,
+  //   title: "3D Solar System Planets to Explore",
+  //   des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+  //   img: "/p1.svg",
+  //   iconLists: [
+  //     "/re.svg",
+  //     "/tail.svg",
+  //     "/ts.svg",
+  //     "/three.svg",
+  //     "/fm.svg",
+  //   ],
+  //   link: "https://github.com/adrianhajdin?tab=repositories",
+  // },
+  // {
+  //   id: 2,
+  //   title: "Yoom - Video Conferencing App",
+  //   des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+  //   img: "/p2.svg",
+  //   iconLists: [
+  //     "/next.svg",
+  //     "/tail.svg",
+  //     "/ts.svg",
+  //     "/stream.svg",
+  //     "/c.svg",
+  //   ],
+  //   link: "https://github.com/adrianhajdin/zoom-clone",
+  // },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: [
+  //     "/re.svg",
+  //     "/tail.svg",
+  //     "/ts.svg",
+  //     "/three.svg",
+  //     "/c.svg",
+  //   ],
+  //   link: "https://github.com/adrianhajdin/ai_saas_app",
+  // },
+
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: [
-      "/re.svg",
-      "/tail.svg",
-      "/ts.svg",
-      "/three.svg",
-      "/fm.svg",
-    ],
-    link: "https://github.com/adrianhajdin?tab=repositories",
-  },
-  {
-    id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
+    title: "Ultimate MovieHub",
+    des: "Ultimate MovieHub is a user-friendly movie site built with React, Redux Toolkit, Material UI, Vite, Firebase, and IMDb. It features a searchable movie catalog, user authentication, and a responsive design for all devices, along with user reviews, ratings, dark/light mode, and personalized recommendations. ",
+
+    img: "/images/movie-cover.png",
     iconLists: [
       "/next.svg",
       "/tail.svg",
       "/ts.svg",
-      "/stream.svg",
-      "/c.svg",
+      "/three.svg",
+      "/gsap.svg",
     ],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    link: "https://movie-app-react-js-a71bd.web.app/",
+    gitHubLink: "https://github.com/hishamrehab/Movies-Website",
   },
+
   {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
+    id: 2,
+    title: "ShopEase Website",
+    des: "Developed a fully functional eCommerce site using React.js, React Router Dom, Vite, Bootstrap, Sass, React Countup Library, and Swiper. The site features a responsive interface, user authentication, product browsing, secure payments, real-time inventory updates, and personalized recommendations for enhanced user engagement. ",
+
+    img: "/images/movie-cover.png",
     iconLists: [
-      "/re.svg",
+      "/next.svg",
       "/tail.svg",
       "/ts.svg",
       "/three.svg",
-      "/c.svg",
+      "/gsap.svg",
     ],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    link: "https://full-ecommerce-website.web.app/",
+    gitHubLink: "https://github.com/hishamrehab/Movies-Website",
   },
+
   {
     id: 4,
     title: "Animated Apple Iphone 3D Website",
