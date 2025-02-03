2174e29f188f9fdb9f9768fdcc06bc0a007ae01d
hishamrehab <hishamrehab30@gmail.com>
2025-02-03 20:40:47 +0200

    Approach section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Projects from "@/components/Projects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
@@ -13,6 +14,7 @@ export default function Home() {
         <Grid />
         <Projects />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
